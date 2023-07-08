9f2971b6d342b812bec439da73cdc9e31be75e9d
sekar N <sekarnagarajan1234@gmail.com>
2023-07-08 22:28:36 +0530

    js

--- Image Search App/script.js
+++ Image Search App/script.js
@@ -10,11 +10,13 @@ let page = 1;
 
 async function searchImage() {
   inputData = inputEl.value;
-  const url = `https://unsplash.com/search/photos?page = ${page}&query = ${inputData}&client_id=${accessKey}`;
+  const url = `https://api.unsplash.com/search/photos?page = ${page}&query = ${inputData}&client_id=${accessKey}`;
   // console.log(url);
 
   const response = await fetch(url);
   const data = await response.json();
+
+  const results = data.results;
 
   if (page === 1) {
     searchResultEl.innerHTML = "";
@@ -35,6 +37,29 @@ async function searchImage() {
     imageLink.href = result.links.html;
     imageLink.target = "_blank";
     imageLink.textContent = result.alt_description;
+
+    //append
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResultEl.appendChild(imageWrapper);
   });
+
+  page++;
+
+  if (page > 1) {
+    showMore.style.display = "block";
+  }
 }
-searchImage();
+
+//Events
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  page = 1;
+  searchImage();
+});
+
+showMore.addEventListener("click", () => {
+  searchImage();
+});
