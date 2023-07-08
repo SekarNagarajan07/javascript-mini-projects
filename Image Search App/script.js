a2e611b1e732196cb12d7fbd1833825617398b3a
sekar N <sekarnagarajan1234@gmail.com>
2023-07-08 21:16:28 +0530

    js

---
+++ Image Search App/script.js
@@ -0,0 +1,6 @@
+const accessKey = "XsfQFd7q5D_eqa43PaFoAIud2FQGTTIDF0lziA8FIzY";
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResultEl = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
