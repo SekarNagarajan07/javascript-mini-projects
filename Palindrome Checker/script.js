bab652bfa02eb3392514ac15321d288ef92c038b
sekar N <sekarnagarajan1234@gmail.com>
2023-07-27 22:48:48 +0530

    js

--- Palindrome Checker/script.js
+++ Palindrome Checker/script.js
@@ -1,13 +1,23 @@
 const txtInput = document.querySelector(".inputs input");
-checkbox = document.querySelector(".inputs button");
+checkBtn = document.querySelector(".inputs button");
 infoTxt = document.querySelector(".info-txt");
 let filterInput;
+
+checkBtn.addEventListener("click", () => {
+  let reverseInput = filterInput.split("").reverse().join("");
+  infoTxt.style.display = "block";
+  if (filterInput != reverseInput) {
+    return (infoTxt.innerHTML = `No , <span>${txtInput.value}</span> is not a palindrome`);
+  } else {
+    infoTxt.innerHTML = `Yes , <span>${txtInput.value}</span> is a palindrome`;
+  }
+});
 
 txtInput.addEventListener("keyup", () => {
   filterInput = txtInput.value.toLowerCase().replace(/[^A-z0-9]/gi, "");
   if (filterInput) {
     return checkBtn.classList.add("active");
   }
-
+  infoTxt.style.display = "none";
   checkBtn.classList.remove("active");
 });
